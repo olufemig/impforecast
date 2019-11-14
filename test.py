a3c3e2202673f4fc71ba66cf4f2fda0533d6323a
olufemig <olufemig@outlook.com>
2019-11-14 22:24:03 +0000

    added db details

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-msg = "Hello World bros"
-print(msg)
+import pyodbc
+import pandas as pd
+
+server = 'tcp:demoserver01.database.windows.net' 
+database = 'demosql' 
+username = 'olufemig' 
+password = 'Just4youdawn' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+crsr = cnxn.cursor()
+for table_name in crsr.tables(tableType='TABLE'):
+    print(table_name)
+cursor = cnxn.cursor()
+sql = "Select *"
+sql = sql + " From data"
+print(sql)
+cursor.execute(sql)
+data = pd.read_sql(sql, cnxn)
