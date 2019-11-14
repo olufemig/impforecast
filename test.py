699887f815d99c61685e257b354dd609b05fb6c7
olufemig <olufemig@outlook.com>
2019-11-14 23:30:22 +0000

    more

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pyodbc
 import pandas as pd
 
-dfSales = pd.read_csv('data/clean_data.csv',header=0, index_col=None)
-dfSales.head()
+dfSales = pd.read_csv('data/format1.csv',header=0, index_col=None)
+dfSales.describe()
+msg = "hello"
+print(msg)
