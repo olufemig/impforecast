9f2bbb54920a984d702d61ec3604c6a5fcb5be8b
olufemig <olufemig@outlook.com>
2019-11-28 13:55:22 +0000

    removed stores with no sales

--- test.py
+++ test.py
@@ -13,15 +13,16 @@ from azureml.core.experiment import Experiment
 from datetime import datetime
 from azureml.train.automl import AutoMLConfig
 from azureml.train.estimator import Estimator
-
+# read date and include weekday flag
 time_column_name = 'dia_date'
 df = pd.read_csv('data/clean_data.csv',parse_dates=[time_column_name])
-dfSales = df[['CUSTOMER_ID','BRANDPACK_ID','Sales_Vol','dia_date']]
-dfSales.Sales_Vol = pd.to_numeric(dfSales.Sales_Vol, errors='coerce').fillna(0, downcast='infer')
+df['weekday']=np.where(((df['dia_date']).dt.dayofweek) < 5,0,1)
+df.Sales_Vol = pd.to_numeric(df.Sales_Vol, errors='coerce').fillna(0, downcast='infer')
+dfSales = df[['dia_date','weekday','CUSTOMER_ID','BRANDPACK_ID','Sales_Vol']]
 
 print(dfSales.dtypes)
 
-
+# add missing dates
 shape2 = dfSales.set_index(['dia_date', 'BRANDPACK_ID', 'CUSTOMER_ID']).\
   unstack([1, 2]).\
   resample('D').asfreq().\
@@ -29,13 +30,19 @@ shape2 = dfSales.set_index(['dia_date', 'BRANDPACK_ID', 'CUSTOMER_ID']).\
   stack([1, 2]).\
   reset_index()
   
-ZeroSales = dfSales[dfSales.Sales_Vol == 0.00]
+ 
 
-#use this to determine all stores with product sales
-NonZeroSales = dfSales[dfSales.Sales_Vol != 0.00]
-df1 = NonZeroSales[['CUSTOMER_ID','BRANDPACK_ID']]
-NonZeroDuplicates = df1.drop_duplicates()
+# use this to determine all stores with no product sales
+AllZeroSales = dfSales[dfSales.Sales_Vol == 0.00]
+df1 = AllZeroSales[['CUSTOMER_ID','BRANDPACK_ID']]
+StoresWithNoProdSales = df1.drop_duplicates()
 
+print(AllZeroSales.head(10))
+
+#Store/Product comnbos with sales
+l=['CUSTOMER_ID','BRANDPACK_ID']
+StoresWithProductsData=shape2.merge(StoresWithNoProdSales[l],on=l,indicator=True,how='outer').loc[lambda x : x['_merge']=='left_only'].copy() 
+swp_csv = StoresWithProductsData.to_csv ('data/stores_with_product.csv', index = None, header=True) 
   
 print('%d unique dates in original df' % dfSales['dia_date'].nunique())
 print('%d rows in original df' % len(dfSales))
@@ -43,9 +50,9 @@ print('%d unique sales volumes in original df' % dfSales['Sales_Vol'].nunique())
 print('%d unique sales volume after filling missing values' % shape2['Sales_Vol'].nunique())
 print('%d unique dates after filling missing values' % shape2['dia_date'].nunique())
 print('%d rows in dataframe after filling missing values' % len(shape2))
-print('%d rows in dataframe with zero sales ' % len(ZeroSales))
-print('%d rows in dataframe with non-zero sales ' % len(NonZeroSales))
-print(NonZeroDuplicates.head(30))
+#print('%d rows in dataframe with zero sales ' % len(ZeroSales))
+#print('%d rows in dataframe with non-zero sales ' % len(NonZeroSales))
+print(StoresWithProductsData.head(10))
 #print(dfSales.groupby('Sales_Vol').count())
 
 
